19b0a120cc5f84dd6396fd522fc3bcafeaec442a
tanvir <tanvirprhasan@gmail.com>
2025-02-18 23:23:32 +0100

    navbar set

--- client/src/pages/Shared/NavBar/NavBar.jsx
+++ client/src/pages/Shared/NavBar/NavBar.jsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 import { FaBars, FaUserCircle, FaHome, FaInfoCircle, FaEnvelope, FaUserCog } from "react-icons/fa";
-
 import useAdmin from "../../../hooks/useAdmin";
 
 const NavBar = () => {
@@ -57,66 +56,46 @@ const NavBar = () => {
           </Link>
         </li>
       )}
-      {user ? (
-        <>
-          <button
-            onClick={handleLogOut}
-            className="btn btn-ghost bg-red-500 text-white hover:bg-red-600"
-          >
-            Log Out
-          </button>
-        </>
-      ) : (
-        <li>
-          <Link to="/login" className="btn btn-outline btn-primary">
-            Login
-          </Link>
-        </li>
-      )}
     </>
   );
 
   return (
-    <div className="navbar bg-opacity-90 bg-black text-white shadow-lg">
-      {/* Navbar Start */}
-      <div className="navbar-start">
-        {/* Dropdown for Mobile */}
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <FaBars className="h-6 w-6" />
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 text-black"
-          >
-            {navOptions}
-          </ul>
-        </div>
-        <div className="flex  justify-start">
-        <Link to="/" className="btn btn-ghost normal-case text-2xl font-bold">
-         MitromoneY
-          </Link>
-          <div className="h-12 w-12 rounded-3xl">
-            <img src="../../../../public/logo.png" alt="" />
+    <div className="bg-black fixed top-0 left-0 w-full z-50 shadow-lg">
+      <div className="navbar container mx-auto px-4 text-white flex justify-between">
+        
+        {/* Left Side: Logo & Name */}
+        <div className="flex items-center gap-2">
+          <div className="h-12 w-12 rounded-full overflow-hidden">
+            <img src="../../../../public/logo.png" alt="Logo" className="object-cover w-full h-full" />
           </div>
+          <Link to="/" className="text-2xl font-bold">MitromoneY</Link>
         </div>
-      </div>
 
-      {/* Navbar Center */}
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{navOptions}</ul>
-      </div>
+        {/* Center: Navigation Links */}
+        <div className="hidden lg:flex">
+          <ul className="menu menu-horizontal px-4">{navOptions}</ul>
+        </div>
 
-      {/* Navbar End */}
-      <div className="navbar-end">
-        {!user && (
-          <Link
-            to="/register"
-            className="btn bg-yellow-500 hover:bg-yellow-600 text-black"
-          >
-            Get Started
-          </Link>
-        )}
+        {/* Right Side: Login/Logout */}
+        <div className="flex items-center">
+          {user ? (
+            <button
+              onClick={handleLogOut}
+              className="btn bg-red-500 text-white hover:bg-red-600"
+            >
+              Log Out
+            </button>
+          ) : (
+            <>
+              <Link to="/login" className="btn btn-outline btn-primary mx-2">
+                Login
+              </Link>
+              <Link to="/register" className="btn bg-yellow-500 hover:bg-yellow-600 text-black">
+                Get Started
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
